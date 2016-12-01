8a6ddc60066db1472d8dfb52c773f772166eabc5
Diego Suarez and Margie Nevarez <margie@>
2016-12-01 09:41:40 -0800

    add mailing and phone method

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,12 +1,37 @@
 class Contact
-
-attr_reader(:first_name, :last_name, :job_title, :company)
+attr_accessor(:first_name, :last_name, :job_title, :company, :mailing_address, :phone_numbers)
 
   define_method(:initialize) do |parameters|
     @first_name = parameters.fetch(:first_name)
     @last_name = parameters.fetch(:last_name)
     @job_title = parameters.fetch(:job_title)
     @company = parameters.fetch(:company)
+    @mailing_address = []
+    @phone_numbers = []
+  end
 
+  define_method(:add_mailing) do |parameters|
+    street = parameters.fetch(:street)
+    city = parameters.fetch(:city)
+    state = parameters.fetch(:state)
+    zip = parameters.fetch(:zip)
+    type = parameters.fetch(:type)
+    @mailing_address.push({:street => street, :city => city, :state => state, :zip => zip, :type => type})
+  end
+
+
+  define_method(:add_phone) do |parameters|
+    area_code = parameters.fetch(:area_code)
+    number = parameters.fetch(:number)
+    type = parameters.fetch(:type)
+    @phone_numbers.push({:area_code => "503", :number => '887-8899', :type => "home"})
   end
 end
+
+
+
+# test_contact = Contact.new({:first_name => "Colin", :last_name => "Farrell", :job_title => "Actor", :company => "Hollywood" })
+# test_contact.add_mailing({:street => '123 Happy Lane', :city => 'Seattle', :state => 'Washington', :zip => '98234', :type => 'home' })
+# test_contact.add_mailing({:street => 'Another street', :city => 'Portland', :state => 'Oregon', :zip => '123123', :type => 'home' })
+#
+# p test_contact.mailing_address[1].fetch(:street)
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -11,4 +11,19 @@ describe(Contact) do
       expect(test_contact.company).to(eq("Hollywood"))
     end
   end
+  describe('#add_mailing') do
+    it "will add a new mailing address to the contact" do
+      test_contact = Contact.new({:first_name => "Colin", :last_name => "Farrell", :job_title => "Actor", :company => "Hollywood" })
+      test_contact.add_mailing({:street => '123 Happy Lane', :city => 'Seattle', :state => 'Washington', :zip => '98234', :type => 'home' })
+      expect(test_contact.mailing_address[0].fetch(:street)).to(eq('123 Happy Lane'))
+    end
+  end
+  describe("#add_phone") do
+    it "add a new phone number for the contact" do
+      test_contact = Contact.new({:first_name => "Colin", :last_name => "Farrell", :job_title => "Actor", :company => "Hollywood" })
+      test_contact.add_phone({:area_code => "503", :number => '887-8899', :type => "home"})
+      expect(test_contact.phone_numbers[0].fetch(:type)).to(eq('home'))
+
+    end
+  end
 end
